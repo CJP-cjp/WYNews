8d3b5564bef7b12194e7903ea71d9f22d49f8c5f
CJP-cjp <chengjingpo624@gmail.com>
2016-09-01 22:46:19 +0800

    功能分析，项目准备（头像，启动图片等），首页面的搭建

---
+++ 网易新闻/网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻
+//
+//  Created by mac on 16/9/1.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网易新闻/网易新闻/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  网易新闻
+//
+//  Created by mac on 16/9/1.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
